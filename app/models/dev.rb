b0aa8729a86fe7662c1e26f4acbe0d7c8868014f
Vincent Makokha <makokhavomondi@gmail.com>
2022-10-31 21:33:27 +0300

    Probably completed

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.find do |freebie|
+            freebie.name == item_name
+        end
+    end
+
+    def give_away(dev, freebie)
+        #I didn't really understand this question so what I wrote might be wrong
+        freebie.dev = dev
+    end
 end
